5d90910693a3e610bcdf02fb65b7bd9835deca44
phuonganh2710 <80328714+phuonganh2710@users.noreply.github.com>
2026-09-13 21:03:16 +0900

    Add: reorder-linked-list - submission-5

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-5.java
@@ -0,0 +1,84 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        // reverse the linkedlist from mid -> tail
+        // find the list length
+        // int len = 0;
+        ListNode ptr = head;
+        ListNode mid = head;
+        ListNode prev = null;
+        while (ptr != null && ptr.next != null){
+            ptr = ptr.next.next;
+            prev = mid;
+            mid = mid.next;
+        }
+        
+        if (prev != null) prev.next = null;
+        else return;
+        ListNode reversedRight = reverseList(mid);
+        //System.out.println("ReversedRight is: ");
+        // printList(reversedRight);
+        //System.out.println("IntactLeft is: ");
+        // printList(head);
+
+        // sandwich the head -> mid into the reversed linkedlist
+        head = mergeList(head, reversedRight);
+        //printList(res);
+    }
+    public void printList(ListNode node){
+        while (node != null){
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        //System.out.println();
+    }
+
+    public ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode ptr = head;
+        ListNode tmp;
+        while (ptr != null){
+            tmp = ptr.next;
+            ptr.next = prev;
+            prev = ptr;
+            ptr = tmp;
+        }
+        return prev;
+    }
+
+    public ListNode mergeList(ListNode list1, ListNode list2){
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        ListNode head = list1;
+        ListNode ptr = head;
+        list1 = list1.next;
+        int last = 1;
+        while (list1 != null || list2 != null){
+            if (list1 == null || last == 1) {
+                ptr.next = list2;
+                ptr = ptr.next;
+                if (list2 != null) list2 = list2.next;
+                last = 2;
+                continue;
+            }
+            if (list2 == null || last == 2) {
+                ptr.next = list1;
+                ptr = ptr.next;
+                if (list1 != null) list1 = list1.next;
+                last = 1;
+                continue;
+            }
+        }
+        return head;
+    }
+}
